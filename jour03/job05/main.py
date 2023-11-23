ff24d90aa482197d82a19375a0b09c5038548691
Ethan <ethan.zieba@laplateforme.io>
2023-11-23 09:18:10 +0100

    Updated job05 with a handling of division by 0

--- jour03/job05/main.py
+++ jour03/job05/main.py
@@ -9,6 +9,10 @@ def calcule(num1, operator, num2):
         case "*":
             return num1 * num2
         case "/":
+            #Using any() function that returns a boolean if any condition in the list is matched
+            if any([num1 == 0, num2 == 0]):
+                #Raising a ValueError that terminates the program, we could also use a simple "return"
+                raise ValueError("Can't divide by 0")
             return num1 / num2
         case "%":
             return num1 % num2
@@ -16,3 +20,5 @@ def calcule(num1, operator, num2):
 print(calcule(2, "*", 4))
 print(calcule(5, "%", 4))
 print(calcule(19, "-", 4))
+print(calcule(19, "/", 2))
+print(calcule(19, "/", 0))
